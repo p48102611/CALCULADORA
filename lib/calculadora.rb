4f7f0b5929da7ffff5036fcd34eba31a8ce638c6
Pablo Alvarez <79346499+p48102611@users.noreply.github.com>
2021-04-29 00:47:48 -0400

    es capaz de reconocer parte delimitador y parte secuencia de numeros

--- lib/calculadora.rb
+++ lib/calculadora.rb
@@ -1,10 +1,23 @@
 def calcularCadena(cad)
-    tmp = cad.gsub("\n",",").split(",")
-    sum = 0
-    tmp.each do |j|
-        sum += j.to_i
+
+
+    if(cad[1] != "/")
+        tmp = cad.gsub("\n",",").split(",")
+        sum = 0
+        tmp.each do |j|
+            sum += j.to_i
+        end
+        return sum
+    else
+        tmp = cad.split("\n")
+        tmp_left = tmp[0]
+        tmp_right = tmp[1].split(";")
+        sum = 0
+        tmp_right.each do |j|
+            sum += j.to_i
+        end
+        return sum
     end
-    return sum
     # tmp = ""
     # array_num = [0]
     # cad.each do |j|
--- spec/calculadora_spec.rb
+++ spec/calculadora_spec.rb
@@ -12,13 +12,25 @@ RSpec.describe "calculadora" do
   it "deberia devolver el numero 9 para la cadena '5,1,3'" do
     expect(calcularCadena("5,1,3")).to eq 9
   end
-  it "deberia devolver el numero 9 para la cadena '5,1,3'" do
+  it "deberia devolver el numero 9 para la cadena '5,1,3,1'" do
     expect(calcularCadena("5,1,3,1")).to eq 10
+  end
+  it "deberia devolver el numero 16 para la cadena '5,1,3\n10'" do
+    expect(calcularCadena("5,1\n10")).to eq 16
   end
   it "deberia devolver el numero 9 para la cadena '5,1\n3'" do
     expect(calcularCadena("5,1\n3")).to eq 9
   end
-#   it "deberia devolver el numero 29 para la cadena '5,1,3,10'" do
-#     expect(calcularCadena("5,1,3,10")).to eq 29
-#   end
+  it "deberia devolver el numero 108 para la cadena '5,100\n3'" do
+    expect(calcularCadena("5,100\n3")).to eq 108
+  end
+  it "deberia devolver el numero 109 para la cadena '5,100\n3,1'" do
+    expect(calcularCadena("5,100\n3,1")).to eq 109
+  end
+  it "deberia devolver el numero 109 para la cadena '5,100\n3,10'" do
+    expect(calcularCadena("5,100,3,10")).to eq 118
+  end
+  it "deberia devolver el numero 109 para la cadena '//[;]\n5;100;3;1'" do
+    expect(calcularCadena("//[;]\n5;100;3;1")).to eq 109
+  end
 end
