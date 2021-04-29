3d3335dd6ba1bca2c9408b4a51288112b94bee82
Pablo Alvarez <79346499+p48102611@users.noreply.github.com>
2021-04-28 22:54:47 -0400

    es capaz de sumar con salto de linea

--- lib/calculadora.rb
+++ lib/calculadora.rb
@@ -1,9 +1,19 @@
 def calcularCadena(cad)
-
-    tmp = cad.split(',')
+    tmp = cad.gsub("\n",",").split(",")
     sum = 0
     tmp.each do |j|
-        sum += j.to_i 
+        sum += j.to_i
     end
     return sum
+    # tmp = ""
+    # array_num = [0]
+    # cad.each do |j|
+    #     if j.to_i >= 0 && j.to_i <= 9
+    #         tmp.join(j)
+    #     else
+    #         array_num.insert(tmp.to_i)
+    #         tmp = ""
+    #     end
+    # end
+    # return sum(array_num)
 end
--- spec/calculadora_spec.rb
+++ spec/calculadora_spec.rb
@@ -9,7 +9,16 @@ RSpec.describe "calculadora" do
   it "deberia devolver el numero 6 para la cadena '5,1'" do
     expect(calcularCadena("5,1")).to eq 6
   end
-  it "deberia devolver el numero 6 para la cadena '5,1,3'" do
+  it "deberia devolver el numero 9 para la cadena '5,1,3'" do
     expect(calcularCadena("5,1,3")).to eq 9
   end
+  it "deberia devolver el numero 9 para la cadena '5,1,3'" do
+    expect(calcularCadena("5,1,3,1")).to eq 10
+  end
+  it "deberia devolver el numero 9 para la cadena '5,1\n3'" do
+    expect(calcularCadena("5,1\n3")).to eq 9
+  end
+#   it "deberia devolver el numero 29 para la cadena '5,1,3,10'" do
+#     expect(calcularCadena("5,1,3,10")).to eq 29
+#   end
 end
